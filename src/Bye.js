3e943c6d45df1d5ff056c0cab2ba9a94a729314b
simran <Simranjit.Kaur2@metropolia.fi>
2024-05-18 00:17:13 +0530

    part3 finished

---
+++ src/Bye.js
@@ -0,0 +1,8 @@
+// Bye.js
+import React from 'react';
+
+function Bye() {
+  return <p>Goodbye, React!</p>;
+}
+
+export default Bye;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+// Hello.js
+import React from 'react';
+
+function Hello() {
+  return <p>Hello, React!</p>;
+}
+
+export default Hello;
